7917775e5ca7572075f77af86040d4dd5a4884a1
dashi-superai <denysyudakov@gmail.com>
2025-10-31 16:59:56 -0400

    Add root-level files

---
+++ start.py
@@ -0,0 +1,40 @@
+import os
+try:
+    import multiprocessing
+    from termcolor import colored
+except ImportError:
+    print('Installing Requirements...')
+    os.system('pip3 install -r requirements.txt')
+    print('Requirements Installed...')
+    print('Please Run Again...')
+
+
+def start_frontend():
+    print(colored('[x] Starting Frontend...', 'blue'))
+    os.chdir('Frontend')
+    os.system('python3 -m http.server 3000')
+    print(colored('[x] Frontend Started...\n', 'blue'))
+    print(colored('[x] Open http://localhost:3000', 'green'))
+
+
+def start_backend():
+    print(colored('[x] Starting Backend...', 'blue'))
+    os.chdir('Backend')
+    print(colored('[x] Installing Requirements...', 'blue'))
+    os.system('pip3 install -r requirements.txt')
+    print(colored('[x] Requirements Installed...', 'blue'))
+    os.system('python3 app.py')
+    print(colored('[x] Backend Started...', 'green'))
+
+
+def main():
+    frontend_p = multiprocessing.Process(target=start_frontend)
+    backend_p = multiprocessing.Process(target=start_backend)
+
+    # Start the processes
+    frontend_p.start()
+    backend_p.start()
+
+
+if __name__ == '__main__':
+    main()
